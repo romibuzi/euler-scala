1c0e49b2aa9712419c647a280bc3f33bf1234181
Romain Ardiet <romain.ardiet@gmail.com>
2018-04-24 00:49:08 +0200

    Problem 28

---
+++ src/main/scala/P028.scala
@@ -0,0 +1,30 @@
+import scala.annotation.tailrec
+
+object P028 {
+  /*
+    21 22 23 24 25
+    20  7  8  9 10
+    19  6  1  2 11
+    18  5  4  3 12
+    17 16 15 14 13
+
+    1st spiral: 3 5 7 9   => incremented by 2
+    2nd spiral: 13 17 21 25 => incremented by 4
+    3rd spiral will be: 31 37 43 49 => incremented by 6
+    etc
+
+    all spirals are 4 numbers length
+   */
+  @tailrec
+  def nextSpiralSum(previousSpiralsSum: Int = 1,
+                    previousSpiralLast: Int = 1,
+                    increment: Int = 2): Int = {
+    if (previousSpiralLast >= MathHelper.square(1001)) return previousSpiralsSum
+
+    val spiral = (1 to 4).scanLeft(previousSpiralLast) {
+      (previous, _) => previous + increment
+    }.drop(1)
+
+    nextSpiralSum(previousSpiralsSum + spiral.sum, spiral.last, increment + 2)
+  }
+}
---
+++ src/test/scala/P028Spec.scala
@@ -0,0 +1,7 @@
+import org.scalatest._
+
+class P028Spec extends FlatSpec with Matchers {
+  "P028 " should "find the sum of the numbers on the diagonals in a 1001 by 1001 spiral " in {
+    P028.nextSpiralSum() shouldBe 669171001
+  }
+}
